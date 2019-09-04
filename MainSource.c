c67a71ed12169e3f1ac7c534cd10d1efb1da29ac
Rubyliss <alexisoko0@gmail.com>
2019-09-04 19:26:15 +0200

    Exo 1 terminé

---
+++ MainSource.c
@@ -0,0 +1,25 @@
+/* Ce programme affiche un message à l'écran */
+
+
+/* accès au bibliotèque des fonction */
+# include <stdio.h>
+# include <stdlib.h>
+
+
+
+main() // Procédure principal + autre forme de commentaire
+{
+
+	// Affichage d'un message à l'écran avec printf + affichage du message "Appuyer pour fermer" avec system
+	//printf("Bonjour à tous\n");
+	//system("pause");
+
+
+	printf("\t\t\t\t\t\tDebut du programme\n\n");
+	printf("====================\n");
+	printf("= Merci et a bientot =\n");
+	printf("====================\n\n");
+	printf("\t\t\t\t\t\tDebut du programme\n\n");
+
+	system("pause");
+}
