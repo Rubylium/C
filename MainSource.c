56aed39b74d78bc5828c337a5f6b7fe3038a3233
Rubylium <alexisoko0@gmail.com>
2019-09-06 08:37:23 +0200

    Ajout d'une entré de valeur

--- MainSource.c
+++ MainSource.c
@@ -7,19 +7,24 @@
 
 
 
+/* Declaration des variables */
+
+int a;
+//int b;
+
+
+
+
 main() // Procédure principal + autre forme de commentaire
 {
-
-	// Affichage d'un message à l'écran avec printf + affichage du message "Appuyer pour fermer" avec system
-	//printf("Bonjour à tous\n");
-	//system("pause");
-
-
-	printf("\t\t\t\tDebut du programme\n\n");
-	printf("\t====================\n");
-	printf("\t= Merci et a bientot =\n");
-	printf("\t====================\n\n");
-	printf("\t\t\t\tFin du programme\n\n");
+	// Calcule de A
+	a = 1;
+	//printf("contenu de la variable a = %d", a ,"\n");
+	printf("Saisir la valeur de a ");
+	scanf("%d", &a);
+	a = a + 5;
+	system("color F");
+	printf("Valeur de a = %d\n", a);
 
 	system("pause");
 }
